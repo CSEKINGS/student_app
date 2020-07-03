effbb817650ac75652f0bdb8f9fadaa8b9e7db16
Harish <warriorharish95668@gmail.com>
2020-07-03 18:52:38 +0530

    added login and onboarding page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+import 'login.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,23 +18,116 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.lightBlue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: OnBoardingPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class OnBoardingPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _OnBoardingPageState createState() => _OnBoardingPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _OnBoardingPageState extends State<OnBoardingPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => LoginPage()),
+    );
+  }
+
+  Widget _buildImage(String assetName) {
+    return Align(
+      child: Image.asset('assets/$assetName.png', width: 350.0),
+      alignment: Alignment.bottomCenter,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Student app'),
+    const bodyStyle = TextStyle(fontSize: 19.0);
+    const pageDecoration = const PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      pages: [
+        PageViewModel(
+          title: "Timetables",
+          body: "Get up-to-date timetables in single click.",
+          image: _buildImage('timetable'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Events",
+          body: "Learn about events and fest.",
+          image: _buildImage('candidate'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Grades",
+          body: "Check your grades and assessment exams scores.",
+          image: _buildImage('grade'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Announcements",
+          body: "Get latest news about the college via push notification.",
+          image: _buildImage('announce'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Notes and Question banks",
+          body: "View and download notes prepared by your college on the go",
+          image: _buildImage('notes'),
+          footer: RaisedButton(
+            onPressed: () {
+              introKey.currentState?.animateScroll(0);
+            },
+            child: const Text(
+              'sample btn',
+              style: TextStyle(color: Colors.white),
+            ),
+            color: Colors.deepPurpleAccent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "placeholder",
+          bodyWidget: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("Lets go ", style: bodyStyle),
+            ],
+          ),
+          image: _buildImage('announce'),
+          decoration: pageDecoration,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      skipFlex: 0,
+      nextFlex: 0,
+      skip: const Text('Skip'),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
       ),
     );
   }
