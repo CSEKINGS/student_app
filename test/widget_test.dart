014f914758225d813bd07a00426e912f12ca2a76
Harish <warriorharish95668@gmail.com>
2020-07-09 10:52:47 +0530

    moved main.dart to root

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'file:///C:/Users/Harish/AndroidStudioProjects/student_app/lib/common/main.dart';
+import 'package:student_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
