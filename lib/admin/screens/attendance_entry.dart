cbf5245e2f41fcb6a1caaa485e7f1298425061bb
praveenmarish <60656910+praveenmarish@users.noreply.github.com>
2020-07-25 14:28:51 +0530

    my contents

--- lib/admin/screens/attendance_entry.dart
+++ lib/admin/screens/attendance_entry.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:student_app/attendance/Designs.dart';
 
 class AttendanceEntry extends StatefulWidget {
   @override
@@ -12,6 +13,40 @@ class _AttendanceEntryState extends State<AttendanceEntry> {
       body: Center(
         child: Text('Attendance entry'),
       ),
+      drawer: Container(
+        width: 210,
+        child: Drawer(
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                 gradient: LinearGradient(colors: <Color>[
+                   Colors.lightBlue,
+                   Colors.lightBlueAccent
+                  ])
+                 ),
+                 child: Container(
+                   child: Column(
+                     mainAxisAlignment: MainAxisAlignment.center,
+                     children: <Widget>[
+                      Text('Header',style: TextStyle(fontSize: 20),),
+                      ],
+                   ),
+                 ),
+              ),
+             CustomView('Add class'),
+             CustomView('Add student'),
+             CustomView('Attendance'),
+             CustomView('Add year'),
+             CustomView('Add dep'),
+             CustomView('Delete students'),
+             CustomView('Delete class'),
+             CustomView('Delete department'),
+             CustomView('Delete year'),
+          ],
+        ),
+       ),
+      )
     );
   }
 }
---
+++ lib/attendance/AddDetails.dart
@@ -0,0 +1,114 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'DbAndRefs.dart';
+
+
+// ignore: must_be_immutable
+class AddClass extends StatefulWidget {
+  String yer,dep;
+  AddClass(this.yer,this.dep);
+
+  @override
+  _AddClassState createState() => _AddClassState();
+}
+
+class _AddClassState extends State<AddClass> {
+
+  List<Contents> classes=[];
+  TextEditingController eCtrl = new TextEditingController();
+  String name;
+  Dbref obj=new Dbref();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getClass();
+  }
+
+  void getClass(){
+    clearData();
+    var yer=widget.yer;
+    var dep=widget.dep;
+    CollectionReference getRef;
+    if(yer!=null&&dep!=null){
+     getRef=obj.getDetailRef2(yer, dep);
+    }
+    else if(yer==null&&dep!=null){
+      getRef=obj.getDetailRef(dep);
+    }
+    else if(dep==null&&yer!=null){
+      getRef=obj.getDetailRef(yer);
+    }
+    getRef.snapshots().listen((event) {
+      setState(() {
+        for (int i=0; i<event.documents.length;i++){
+          classes.add(Contents.fromSnapshot(event.documents[i]));
+        }
+      });
+    });
+  }
+
+  void clearData(){
+    setState(() {
+      classes.clear();
+    });
+  }
+
+  void addClassname(String name) {
+    String yer = widget.yer;
+    String dep = widget.dep;
+    CollectionReference addRef;
+    if(yer!=null&&dep!=null){
+      addRef=obj.getDetailRef2(yer, dep);
+    }
+    else if(yer==null&&dep!=null){
+      addRef=obj.getDetailRef(dep);
+    }
+    else if(dep==null&&yer!=null){
+      addRef=obj.getDetailRef(yer);
+    }
+    addRef.add(
+        {
+          'name': name,
+        }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('add class'),
+      ),
+      body:SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            new TextField(
+              controller: eCtrl,
+              onSubmitted: (text) {
+                addClassname(text);
+                eCtrl.clear();
+                clearData();
+                setState(() {});
+              },
+            ),
+
+            new ListView.builder(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                padding: EdgeInsets.all(10),
+                itemCount: classes.length,
+                itemBuilder: (context,int index)=>Container(
+                    child: ListTile(
+                      title: new Text(classes[index].name.toString()),
+                    )
+                )
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
