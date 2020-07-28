197ab781594e275a0078bc8714388fad0bcfa73a
Harish <warriorharish95668@gmail.com>
2020-07-28 21:02:54 +0530

    added grade webview

--- lib/student/screens/grade.dart
+++ lib/student/screens/grade.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class Grade extends StatefulWidget {
   @override
@@ -6,12 +7,30 @@ class Grade extends StatefulWidget {
 }
 
 class _GradeState extends State<Grade> {
+  String url = 'https://coe1.annauniv.edu/home/';
+  bool isLoading = true;
+  final _key = UniqueKey();
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('grade'),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+        body: Stack(
+          children: <Widget>[
+            WebView(
+              key: _key,
+              initialUrl: this.url,
+              javascriptMode: JavascriptMode.unrestricted,
+              onPageFinished: (finish) {
+                setState(() {
+                  isLoading = false;
+                });
+              },
+            ),
+            isLoading
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Stack(),
+          ],
+        ),
+      );
 }
