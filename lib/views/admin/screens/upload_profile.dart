a7527f8ec914090dbbfe8e8efe7025b99f64eb68
Yatendra <yatendra@Yatendras-MacBook-Air.local>
2021-10-06 11:01:50 +0530

    Restructure to MVC

--- lib/views/admin/screens/upload_profile.dart
+++ lib/views/admin/screens/upload_profile.dart
@@ -5,7 +5,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:student_app/admin/Models/db_model.dart';
+import 'package:student_app/models.dart';
 
 class UploadProfile extends StatefulWidget {
   @override
