2d07e261904d7087ba835f134bd41e9a1f327d01
harishanbalagan <harishanbalagan@thirantech.com>
2021-10-11 21:24:25 +0530

    reformatted and removed webview in grade

--- lib/views/student/screens/grade.dart
+++ lib/views/student/screens/grade.dart
@@ -27,46 +27,14 @@ class _GradeState extends State<Grade> {
 
   @override
   Widget build(BuildContext context) {
-    if (kIsWeb) {
-      return const Center(child: Text('Visit https://coe1.annauniv.edu/home/'));
-    } else {
-      return SafeArea(
-        child: Scaffold(
-          body: Stack(
-            children: <Widget>[
-              WebView(
-                key: _key,
-                initialUrl: url,
-                javascriptMode: JavascriptMode.unrestricted,
-                onWebViewCreated: (controller) {
-                  _controller = controller;
-                },
-                onPageFinished: (url) {
-                  _controller.evaluateJavascript(
-                      "document.getElementsByClassName('box')[0].style.display='none';"
-                      "document.getElementsByTagName(\"body\")[0].style.background='none';"
-                      "document.querySelector(\"#slider\").style.display='none';"
-                      "document.querySelector(\"#sidebar > div\").style.display='none';"
-                      "document.querySelector(\"#footer\").style.display='none';"
-                      "document.querySelector(\"#header\").style.display='none';"
-                      "document.querySelector(\"#menufront\").style.display='none';"
-                      "document.getElementById('register_no').value = '${widget.details![2]}';"
-                      "document.getElementById('dob').value = '${widget.details![4]}';");
-
-                  setState(() {
-                    isLoading = false;
-                  });
-                },
-              ),
-              isLoading
-                  ? const Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : Stack(),
-            ],
-          ),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text('TODO Grades'),
+          ],
         ),
-      );
-    }
+      ),
+    );
   }
 }
--- lib/views/student/screens/pdf_api.dart
+++ lib/views/student/screens/pdf_api.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:flutter/services.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
@@ -7,16 +6,12 @@ import 'package:http/http.dart' as http;
 import 'dart:io';
 
 class PDFApi {
-
-
   static Future<File> loadNetwork(String url) async {
     final response = await http.get(Uri.parse(url));
     final bytes = response.bodyBytes;
 
     return _storeFile(url, bytes);
   }
-
- 
 
   static Future<File> _storeFile(String url, List<int> bytes) async {
     final filename = basename(url);
